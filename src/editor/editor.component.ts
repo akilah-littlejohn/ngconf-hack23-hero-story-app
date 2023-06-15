df494a973bfdd3c7a044a74a8450444e06ca8187
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-06-14 22:54:34 -0500

    2 files modified, 5 added

---
+++ src/editor/editor.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css'],
+  standalone:true,
+ imports:[]
+})
+export class EditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
